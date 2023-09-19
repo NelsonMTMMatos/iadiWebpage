9b1e428c03e67e2543a4bdc4e2f0eaa07929b7cb
Nélson <nm.matos@campus.fct.unl.pt>
2023-09-19 23:13:21 +0100

    Added some features

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 import pokemons from './pokemons.json' assert { type: 'json'};
 
+
+fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((response) => response.json()).then((data) => console.log(data))
+
 console.log(pokemons);
