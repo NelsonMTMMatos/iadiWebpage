c076653c7f264fc371cfc84388eba6d5ab6bbfa4
Nélson <nm.matos@campus.fct.unl.pt>
2023-09-20 13:06:14 +0100

    Added modal

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 import pokemons from './pokemons.json' assert { type: 'json'};
 
 
+const closeModal = document.getElementById("close-modal");
+
+
+closeModal.addEventListener('click', () => {
+    
+})
+
+
+
+const img = new Image(300,400);
+img.src = link;
+document.getElementById("screen").appendChild(img)
+
 fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((response) => response.json()).then((data) => console.log(data))
 
 console.log(pokemons);
